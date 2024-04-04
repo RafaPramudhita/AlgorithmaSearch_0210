32b5fb6cbe84655e725e02fab16dba1b7ad4c96e
RafaPramudhita <161433424+RafaPramudhita@users.noreply.github.com>
2024-04-04 15:26:21 +0700

    perulangan perbandingan jika array sama dengan item

--- LinearSearch.cpp
+++ LinearSearch.cpp
@@ -41,6 +41,16 @@ void LinearSearch()
         cin >> item;
         ctr = 0;
         i = 0;      //step 2
+        while (i < n)       //step 3
+        {
+            ctr++;
+            if (arr[i] == item)
+            {
+                
+            }
+            
+        }
+
         
     } 
 }
