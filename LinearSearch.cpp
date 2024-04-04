57657d2de7ed95557a9dedbb04f618c67db8c479
RafaPramudhita <161433424+RafaPramudhita@users.noreply.github.com>
2024-04-04 15:27:25 +0700

    input untuk menemukan posisi

--- LinearSearch.cpp
+++ LinearSearch.cpp
@@ -46,9 +46,10 @@ void LinearSearch()
             ctr++;
             if (arr[i] == item)
             {
-                
+                cout << "\n" << item << " found at positio" << (i + 1) << endl;     //step 6
+                break;
             }
-            
+            i++;       //step 4
         }
 
         
